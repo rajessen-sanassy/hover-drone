696d596ba10128cbf9121c9d96ccdba127070b82
Rajessen Sanassy <kapoolazi@gmail.com>
2023-11-02 20:15:39 -0400

    fix conflict errors
    Co-authored-by: Rajessen Sanassy <kapoolazi@gmail.com>
    Co-authored-by: Josh Kim <joshkimsj@gmail.com>
    Co-authored-by: Osman Mohamed <osmanimohamed@cmail.carleton.ca>
    Co-authored-by: Shirwa Jama <shirwajama978@gmail.com>
    Co-authored-by: Jovin Bains <jovin.bains007@gmail.com>

--- hover_drone_gym/envs/game_logic/drone.py
+++ hover_drone_gym/envs/game_logic/drone.py
@@ -10,27 +10,13 @@ HEIGHT = 500
 BASE_PATH = os.path.realpath("./hover_drone_gym/assets")
 DRONE_IMAGE = os.path.join(BASE_PATH, 'drone.png')
 BG_IMAGE = os.path.join(BASE_PATH, 'background.png')
-import os
-from math import sin, cos, pi
-
-FPS = 60
-WIDTH = 800
-HEIGHT = 500
-BASE_PATH = os.path.realpath("./hover_drone_gym/assets")
-DRONE_IMAGE = os.path.join(BASE_PATH, 'drone.png')
-BG_IMAGE = os.path.join(BASE_PATH, 'background.png')
 
 class Drone(pygame.sprite.Sprite):
     def __init__(self, x, y, screen_width, screen_height, drone_image):
-    def __init__(self, x, y, screen_width, screen_height, drone_image):
         pygame.sprite.Sprite.__init__(self) 
-        self.image = drone_image
         self.image = drone_image
         self.rect = self.image.get_rect()
         self.rect.center = [x,y]
-        self.is_alive = True
-        self.screen_width = screen_width
-        self.screen_height = screen_height
 
         # physics
         # gravity
@@ -45,36 +31,12 @@ class Drone(pygame.sprite.Sprite):
         self.is_alive = True
         self.screen_width = screen_width
         self.screen_height = screen_height
-
-        # physics
-        # gravity
-        self.gravity = 0.08
-
-        # initialize angular movements
-        self.angle = 0
-        self.angular_speed = 0
-        self.angular_acceleration = 0
 
         # initialize velocities and accelerations
         self.velocity_x = 0
         self.acceleration_x = 0
         self.acceleration_x = 0
         self.velocity_y = 0
-        self.acceleration_y = 0
-
-        # propeller thrust to be added upon button presses
-        self.thruster_amplitude = 0.04
-
-        # rate of rotation upon button presses
-        self.diff_amplitude = 0.003
-
-        # Default propeller force
-        self.thruster_default = 0.04
-
-        self.mass = 1
-
-        # Length from center of mass to propeller
-        self.arm = 25
         self.acceleration_y = 0
 
         # propeller thrust to be added upon button presses
@@ -105,45 +67,15 @@ class Drone(pygame.sprite.Sprite):
         thruster_right = self.thruster_default
 
         # Adjusting the thrust based on key press
-
-        # Resetting values
-        self.acceleration_x = 0
-        self.acceleration_y = self.gravity
-        self.angular_acceleration = 0
-        thruster_left = self.thruster_default
-        thruster_right = self.thruster_default
-
-        # Adjusting the thrust based on key press
         if key==0:
-            thruster_left += self.thruster_amplitude
-            thruster_right += self.thruster_amplitude
             thruster_left += self.thruster_amplitude
             thruster_right += self.thruster_amplitude
         if key==1:
             thruster_left -= self.thruster_amplitude
             thruster_right -= self.thruster_amplitude
-            thruster_left -= self.thruster_amplitude
-            thruster_right -= self.thruster_amplitude
         if key==2:
             thruster_left -= self.diff_amplitude
-            thruster_left -= self.diff_amplitude
         if key==3:
-            thruster_right -= self.diff_amplitude
-        
-        total_thrust = thruster_left + thruster_right
-        angle_radian = self.angle * pi / 180
-
-        # calculate x y acceleration based on angle
-        self.acceleration_x += (-(total_thrust) * sin(angle_radian) / self.mass)
-        self.acceleration_y += (-(total_thrust) * cos(angle_radian) / self.mass)
-
-        # negative = right rotation in pygame
-        self.angular_acceleration += self.arm * (thruster_right - thruster_left) / self.mass
-
-        # update velocities
-        self.velocity_x += self.acceleration_x
-        self.velocity_y += self.acceleration_y
-        self.angular_speed += self.angular_acceleration
             thruster_right -= self.diff_amplitude
         
         total_thrust = thruster_left + thruster_right
@@ -170,7 +102,6 @@ class Drone(pygame.sprite.Sprite):
         #updating the positions 
         # self.rect.x += self.velocity_x
         self.rect.y += self.velocity_y
-        self.angle += self.angular_speed
         self.angle += self.angular_speed
 
         #make it stay on the screen and not move off
